62118231f46d530a49cc1de0e45f0c1f1d5bfc8f
danimartinez <dani.martinez85@gmail.com>
2026-03-18 22:14:25 +0100

    fix: remove cloudflare adapter to output standard static files for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
   i18n: {
     defaultLocale: 'ca',
     locales: ['ca', 'es', 'en'],
